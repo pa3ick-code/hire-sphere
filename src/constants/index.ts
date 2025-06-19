561cc3ad0a72c92a0fb32436fd16540f7b0a2678
pa3ick-code <asubiaropatrick@gmail.com>
2025-06-19 02:25:25 +0100

    update ai raalker

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -108,7 +108,7 @@ export const interviewer: CreateAssistantDTO = {
   },
   voice: {
     provider: "11labs",
-    voiceId: "sarah",
+    voiceId: "rachel",
     stability: 0.4,
     similarityBoost: 0.8,
     speed: 0.9,
